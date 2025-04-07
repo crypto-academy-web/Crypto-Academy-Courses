6c28378107f2bba447f071ac1638da0a39dcf1e7
salmanamjad123 <103861241+salmanamjad123@users.noreply.github.com>
2025-04-07 18:19:06 +0500

    Update index.tsx

--- src/components/Contact-us/Write-to-us/index.tsx
+++ src/components/Contact-us/Write-to-us/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Text from "@/components/ui/Text";
 
 const WriteUs = () => {
+  
   return (
     <div className="w-full flex justify-center items-center">
       <div className=" w-full max-w-[1242px] px-5">
