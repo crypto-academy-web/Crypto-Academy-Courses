b56294c65d9082e671b20573fcc70c0d198e861b
Developer <developer@innovativemojo.com>
2025-04-07 18:12:10 +0500

    paid courses

--- src/components/Home/PaidCourse/index.tsx
+++ src/components/Home/PaidCourse/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 import Text from '@/components/ui/Text'
 
@@ -9,7 +10,7 @@ import Button from '@/components/ui/Button';
 const courses = [
     {
         id: 1,
-        imageSrc: course1, 
+        imageSrc: course1,
         title: 'All Assets Trading Course',
         description: 'Start Trading Course Now... Quick & Dirty',
         duration: '80 minutes',
@@ -41,7 +42,7 @@ const courses = [
     },
     {
         id: 5,
-        imageSrc: course1, 
+        imageSrc: course1,
         title: 'All Assets Trading Course',
         description: 'Start Trading Course Now... Quick & Dirty',
         duration: '80 minutes',
@@ -49,7 +50,7 @@ const courses = [
     },
     {
         id: 6,
-        imageSrc: course1, 
+        imageSrc: course1,
         title: 'Beginner Trading Course',
         description: 'Start Trading Course Now... Quick & Dirty',
         duration: '60 minutes',
@@ -57,7 +58,7 @@ const courses = [
     },
     {
         id: 7,
-        imageSrc: course1, 
+        imageSrc: course1,
         title: 'Advanced Trading Strategies',
         description: 'Start Trading Course Now... Quick & Dirty',
         duration: '120 minutes',
@@ -65,7 +66,7 @@ const courses = [
     },
     {
         id: 8,
-        imageSrc: course1, 
+        imageSrc: course1,
         title: 'Crypto Trading Basics',
         description: 'Start Trading Course Now... Quick & Dirty',
         duration: '90 minutes',
@@ -74,10 +75,41 @@ const courses = [
 ];
 
 const PaidCourse = () => {
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedOption(e.target.value);
+    };
+
     return (
         <div className='pt-14 pb-12  bg-[#2026301A] px-5'>
             <div className="max-w-[1313px] w-full mx-auto">
-                <Text as="h2" className=''>Paid Courses</Text>
+                <div className="flex justify-between">
+                    <Text as="h2" className=''>Paid Courses</Text>
+
+                    <div className="w-[302px] h-[57px]">
+                        <div className="relative">
+                            <select
+                                id="dropdown"
+                                value={selectedOption}
+                                onChange={handleChange}
+                                className="mt-2 block w-full px-4 py-2 h-[57px] bg-white border-[0.2px] rounded-none border-[#000000]/20 focus:outline-none appearance-none pr-8 text-[14px] text-primary font-helvetica font-light"
+                            >
+                                <option className='text-[14px] text-primary font-helvetica font-light' value="" disabled>Release date (newest first)</option>
+                                <option value="option1">Option 1</option>
+                                <option value="option2">Option 2</option>
+                                <option value="option3">Option 3</option>
+                                <option value="option4">Option 4</option>
+                            </select>
+                            <span className="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none">
+                                <svg className="w-4 h-4 text-gray-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                </div>
 
                 <div className="w-full max-w-[1313px] mx-auto flex flex-wrap items-center justify-between mob:justify-center gap-8 mt-12">
                     {courses.map((course) => (
